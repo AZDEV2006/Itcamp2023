112c7f1a8f82ef70b3e5aa8c618fb31fbabb6c29
AZDEV2006 <120896393+AZDEV2006@users.noreply.github.com>
2023-04-19 01:47:47 +0700

    Create Loop-test.js

---
+++ Loop-test.js
@@ -0,0 +1,32 @@
+let count = 3; //กำหนดตัวแปรจำนวนที่จะ Loop
+let star = ""; // กำหนดตัวแปรเพื่อที่จะนำไป Output
+for (let i = 1; i <= count ; i++) { //  Loop จำนวน Input (ลูปนอก)
+  for (let j = 0; j < i; j++) {  // (ลูปใน) เอา จำนวน I มา Loop แล้วสร้างตัวแปรใหม่ชื่อว่า "j"
+    star += "*"; //ตรงนี้เป็น Assignment operators นะครับ ที่มาของมันคือ start = start + "*" นั่นเองงง
+  }
+  star += "\n"; // ที่มาของ บรรทัดนี้คือ start = start + "\n" เพื่อเว้นบรรทัด
+} 
+console.log(star); //แสดงผลข้อมูล Terminallllllll 
+
+// Result : 
+// *
+// **
+// ***
+// --------------------------------
+
+let count = 5; //กำหนดตัวแปรจำนวนที่จะ Loop
+let star = ""; // กำหนดตัวแปรเพื่อที่จะนำไป Output
+for (let i = 1; i <= count ; i++) { //  Loop จำนวน Input (ลูปนอก)
+  for (let j = 0; j < i; j++) {  //(ลูปใน) เอา จำนวน I มา Loop แล้วสร้างตัวแปรใหม่ชื่อว่า "j"
+    star += "*"; //ตรงนี้เป็น Assignment operators นะครับ ที่มาของมันคือ start = start + "*" นั่นเองงง
+  }
+  star += "\n"; // ที่มาของ บรรทัดนี้คือ start = start + "\n" เพื่อเว้นบรรทัด
+} 
+console.log(star); //แสดงผลข้อมูล Terminallllllll 
+
+// Resultttt :
+// *
+// **
+// ***
+// ****
+// *****
